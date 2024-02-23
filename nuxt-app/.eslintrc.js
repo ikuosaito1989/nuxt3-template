11fa774d7eb83de644bfa10222f3ac1e3720b79c
ikuosaito1989 <ikuosaito1989@gmail.com>
2024-02-24 00:18:36 +0900

    add eslintrc prittier

---
+++ nuxt-app/.eslintrc.js
@@ -0,0 +1,25 @@
+module.exports = {
+  root: true,
+  env: {
+    browser: true,
+    node: true,
+  },
+  extends: [
+    'eslint:recommended',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:vue/vue3-recommended',
+    'prettier',
+  ],
+  plugins: ['prettier'],
+  // add your custom rules here
+  rules: {
+    'import/order': [
+      'error',
+      {
+        alphabetize: {
+          order: 'asc',
+        },
+      },
+    ],
+  },
+}
