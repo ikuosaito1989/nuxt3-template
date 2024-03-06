6acee4afc0ad2c1eaeeeb990d8f32ea71058dce7
ikuosaito1989 <ikuosaito1989@gmail.com>
2024-03-06 23:17:46 +0900

    add nuxt-simple-sitemap

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -17,7 +17,19 @@ export default defineNuxtConfig({
         )
       })
     },
+    'nuxt-simple-sitemap',
   ],
+  sitemap: {
+    urls: [
+      {
+        url: 'http://localhost:3000/',
+        lastmod: '2024-03-06',
+        priority: 1.0,
+        loc: '',
+      },
+    ],
+    xsl: false,
+  },
   devtools: { enabled: true },
   vite: {
     vue: {
